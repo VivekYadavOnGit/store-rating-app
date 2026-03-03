e754134fb7ac347a5adc7f1389c4bfb047971180
VivekYadavOnGit <vivekyadavatwork@gmail.com>
2026-03-03 21:06:25 +0530

    Fixed CORS Policy

--- store-rating-backend/server.js
+++ store-rating-backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://ratehub.vercel.app"
+    "https://ratehub-web.vercel.app"
   ],
   credentials: true
 }))
