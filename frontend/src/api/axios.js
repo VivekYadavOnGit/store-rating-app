4e8ab3b9ed1b61ef4b36f1b264c858f418cb19ea
VivekYadavOnGit <vivekyadavatwork@gmail.com>
2026-03-03 20:06:33 +0530

    Adding Render Backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ratehub-7dyk.onrender.com/api",
 })
-
 
 API.interceptors.request.use(
   (config) => {
